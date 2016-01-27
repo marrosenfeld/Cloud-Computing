a1d59ebed1d07e473f6af70bd4eee22e80086a16
Martin Rosenfeld <mrosenf4@hawk.iit.edu>
2016-01-27 17:49:37 -0600

    add strategy feature

--- PA1/memory/memoery_test/main.c
+++ PA1/memory/memoery_test/main.c
@@ -16,8 +16,11 @@
 #include <string.h>
 #include <time.h>
 #define ARRAY_SIZE 80000000//150000000 //define an array size bigger than cache
-                  
-void measure(int block_size, const double operations, int threads, double* throughput, double* latency);
+      
+typedef enum {SEQUENTIAL, RANDOM} strategy;
+const char* strategyNames[] = {"Sequential", "Random"};
+void measure(int block_size, const double operations, const int threads, const strategy strategy, double* throughput, double* latency);
+
 
 int main(int argc, char** argv) {
     
@@ -25,9 +28,8 @@ int main(int argc, char** argv) {
     double *throughput, *latency;
     int block_sizes[3] = {1,1024, 1048576};
     int threads[2] = {1,2};
-    double operations[6] = { ARRAY_SIZE, ARRAY_SIZE, 50000000,50000000,100000,100000};
-    
-    int i,j,o;
+    double operations[3] = { ARRAY_SIZE, 50000000,100000};
+    int i,j,o,k;
     
     throughput = (double*)malloc(1*sizeof(double));
     latency = (double*)malloc(1*sizeof(double));
@@ -35,9 +37,11 @@ int main(int argc, char** argv) {
     o=0;
     for(i=0; i<(sizeof(block_sizes)/sizeof(int));i++){
         for(j=0; j<(sizeof(threads)/sizeof(int));j++){
-            measure(block_sizes[i], operations[o], threads[j], throughput, latency);   
-            fprintf(fp, "%i, %i, %f, %f\n",block_sizes[i],threads[j],*throughput, *latency);
-            o++;
+            for(k=SEQUENTIAL; k<=RANDOM; k++){
+                measure(block_sizes[i], operations[i], threads[j], k, throughput, latency);   
+                fprintf(fp, "%i, %i, %i, %f, %f\n",block_sizes[i],threads[j],k,*throughput, *latency);
+                o++;
+            }
         }
     }
     
@@ -45,28 +49,32 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
-void measure(const int block_size, const double operations, const int threads, double* throughput, double* latency){
+void measure(const int block_size, const double operations, const int threads, const strategy strategy, double* throughput, double* latency){
     //source array to copy
     char *source_array = (char*)malloc(ARRAY_SIZE * sizeof(char));
     //target array to copy
     char *target_array = (char*)malloc(ARRAY_SIZE * sizeof(char));
     //times
     clock_t start_time, end_time;
-    int i, block_count;
+    int i, block_count, block_number;
     double total_seconds;
     
     block_count = ARRAY_SIZE / block_size;
     //operations=ARRAY_SIZE/block_size;
     start_time = clock();
-    
+    srand(time(NULL));
+
     for(i = 0; i < operations; i++) 
     {
-        int block_number = i % block_count;
+        if(strategy == SEQUENTIAL)
+            block_number = i % block_count;
+        else
+            block_number = rand() % block_count;
         memcpy(target_array+(block_number*block_size),source_array+(block_number*block_size),block_size);   
     }
     
     end_time = clock();
-    printf("\nBlock size: %d, threads: %d\n", block_size, threads);
+    printf("\nBlock size: %d, threads: %d, strategy: %s\n", block_size, threads, strategyNames[strategy]);
     printf("Total seconds: %f\n", (end_time-start_time)/((double)CLOCKS_PER_SEC));
     printf("Mb: %f\n", (operations*(double)block_size/(double)1048576));
     
